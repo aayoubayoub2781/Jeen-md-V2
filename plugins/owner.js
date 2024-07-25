512c660060bf540a283fac311096c00796e0f294
ayoub-X5 <youssefpoup@gmail.com>
2024-07-26 00:49:13 +0100

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -3,14 +3,12 @@ let handler = async (m, { conn }) => {
   let caption = `
 *「 معلومات عن صاحب البوت 」*
 
-*Number :*\nwa.me/212717457920
-*instagram:*\ninstagram.com/noureddine_ouafy
 
-*youtube:*\nyoutube.com/@noureddineouafy2
+*instagram:*\ninstagram.com/majnon._.98
 
-*facebook page:*\nwww.facebook.com/profile.php?id=100063533185520
+*instagram:*\ninstagram.com/_ayoub_x5
 
-*script bot :* github.com/noureddineouafy
+
 
 `.trim()
   m.reply(caption)
